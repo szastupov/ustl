22372d87e5fa669b1b5b3c11f2ff887323fbb3f2
msharov <msharov>
2005-07-12 17:35:32 +0000

    Added heap algorithms bvt

---
+++ bvt/bvt24.cc
@@ -0,0 +1,69 @@
+// This file is part of the ustl library, an STL implementation.
+//
+// Copyright (C) 2005 by Mike Sharov <msharov@users.sourceforge.net>
+// This file is free software, distributed under the MIT License.
+//
+
+// Tests heap algorithms
+//
+
+#include "stdtest.h"
+
+static void HeapSize (size_t nElements, size_t& layerWidth, size_t& nLayers)
+{
+    layerWidth = 0;
+    nLayers = 0;
+    for (size_t fts = 0; nElements > fts; fts += layerWidth) {
+	layerWidth *= 2;
+	if (!layerWidth)
+	    ++ layerWidth;
+	++ nLayers;
+    }
+}
+
+static void PrintSpace (size_t n)
+{
+    for (uoff_t s = 0; s < n; ++ s)
+	cout << ' ';
+}
+
+static void PrintHeap (const vector<int>& v)
+{
+    size_t maxWidth, nLayers;
+    HeapSize (v.size(), maxWidth, nLayers);
+    vector<int>::const_iterator src (v.begin());
+    cout << ios::width(3);
+    maxWidth *= 3;
+    for (uoff_t i = 0; i < nLayers; ++ i) {
+	const size_t w = 1 << i;
+	const size_t spacing = max (0, int(maxWidth / w) - 3);
+	PrintSpace (spacing / 2);
+	for (uoff_t j = 0; j < w && src != v.end(); ++ j) {
+	    cout << *src++;
+	    if (j < w - 1 && src != v.end() - 1)
+		PrintSpace (spacing);
+	}
+	cout << endl;
+    }
+}
+
+void TestHeapOperations (void)
+{
+    static const int c_Values [31] = {	// 31 values make a full 4-layer tree
+	93, 92, 90, 86, 83, 86, 77, 40, 72, 36, 68, 82, 62, 67, 63, 15,
+	26, 26, 49, 21, 11, 62, 67, 27, 29, 30, 35, 23, 59, 35, 29
+    };
+    vector<int> v;
+    v.reserve (VectorSize(c_Values));
+    for (uoff_t i = 0; i < VectorSize(c_Values); ++ i) {
+	v.push_back (c_Values[i]);
+	push_heap (v.begin(), v.end());
+	cout << "------------------------------------------------" << endl;
+	if (!is_heap (v.begin(), v.end()))
+	    cout << "Is NOT a heap" << endl;
+	PrintHeap (v);
+    }
+}
+
+StdBvtMain (TestHeapOperations)
+
