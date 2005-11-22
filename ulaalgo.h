7668c1297cffb576f55a3c264e23757824f91f8b
msharov <msharov>
2005-11-22 20:54:09 +0000

    Made sure to use the m constraint for everything, and removed volatile

--- ulaalgo.h
+++ ulaalgo.h
@@ -65,36 +65,36 @@ void transpose (matrix<N,N,T>& m)
 template <>
 inline void load_identity (matrix<4,4,float>& m)
 {
-    static const float one (1.0);
-    asm volatile (
-	"movups %1, (%0)		\n\t"	// 1 0 0 0
-	"movaps %1, %%xmm1		\n\t"	// 1 0 0 0
+    asm (
+	"movaps %4, %%xmm1		\n\t"	// 1 0 0 0
+	"movups %4, %0			\n\t"	// 1 0 0 0
 	"shufps $0xB1,%%xmm1,%%xmm1	\n\t"	// 0 1 0 0
-	"movups %%xmm1, 16(%0)		\n\t"	// 0 1 0 0
-	"shufps $0x4F,%1,%%xmm1		\n\t"	// 0 0 1 0
-	"shufps $0x1B,%1,%1		\n\t"	// 0 0 0 1
-	"movups %%xmm1, 32(%0)		\n\t"	// 0 0 1 0
-	"movups %1, 48(%0)"			// 0 0 0 1
-	:
-	: "r"(m.begin()), "x"(one)
-	: "xmm0", "xmm1", "memory"
+	"movups %%xmm1, %1		\n\t"	// 0 1 0 0
+	"shufps $0x4F,%4,%%xmm1		\n\t"	// 0 0 1 0
+	"shufps $0x1B,%4,%4		\n\t"	// 0 0 0 1
+	"movups %%xmm1, %2		\n\t"	// 0 0 1 0
+	"movups %4, %3"				// 0 0 0 1
+	: "=m"(m[0][0]), "=m"(m[1][0]), "=m"(m[2][0]), "=m"(m[3][0])
+	: "x"(1.0f)
+	: "xmm1"
     );
 }
 
 inline void _sse_load_matrix (const float* m)
 {
-    asm volatile (
-	"movups (%0), %%xmm4		\n\t" // xmm4 = m[1 2 3 4]
-	"movups 16(%0), %%xmm5		\n\t" // xmm5 = m[1 2 3 4]
-	"movups 32(%0), %%xmm6		\n\t" // xmm6 = m[1 2 3 4]
-	"movups 48(%0), %%xmm7		\n\t" // xmm7 = m[1 2 3 4]
-	: : "r"(m) : "xmm4", "xmm5", "xmm6", "xmm7"
+    asm (
+	"movups %0, %%xmm4		\n\t" // xmm4 = m[1 2 3 4]
+	"movups %1, %%xmm5		\n\t" // xmm5 = m[1 2 3 4]
+	"movups %2, %%xmm6		\n\t" // xmm6 = m[1 2 3 4]
+	"movups %3, %%xmm7		\n\t" // xmm7 = m[1 2 3 4]
+	: : "m"(m[0]), "m"(m[4]), "m"(m[8]), "m"(m[12])
+	: "xmm4", "xmm5", "xmm6", "xmm7"
     );
 }
 
 inline void _sse_transform_to_vector (float* result)
 {
-    asm volatile (
+    asm (
 	"movaps %%xmm0, %%xmm1		\n\t" // xmm1 = t[0 1 2 3]
 	"movaps %%xmm0, %%xmm2		\n\t" // xmm1 = t[0 1 2 3]
 	"movaps %%xmm0, %%xmm3		\n\t" // xmm1 = t[0 1 2 3]
@@ -109,35 +109,34 @@ inline void _sse_transform_to_vector (float* result)
 	"mulps  %%xmm7, %%xmm3		\n\t" // xmm3 = t[3 3 3 3] * m[0 1 2 3]
 	"addps  %%xmm3, %%xmm2		\n\t" // xmm2 = xmm2 + xmm3
 	"addps  %%xmm2, %%xmm0		\n\t" // xmm0 = result
-	"movups %%xmm0, (%0)"
-	:
-	: "r"(result)
-	: "xmm0", "xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "xmm7", "memory"
+	"movups %%xmm0, %0"
+	: "=m"(result[0]) :
+	: "xmm0", "xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "xmm7"
     );
 }
 
 template <>
-inline tuple<4,float> operator* (const tuple<4,float>& t, const matrix<4,4,float>& m)
+static tuple<4,float> operator* (const tuple<4,float>& t, const matrix<4,4,float>& m)
 {
     tuple<4,float> result;
     _sse_load_matrix (m.begin());
-    asm volatile ("movups %0, %%xmm0	\n\t" : : "m"(t[0]) : "xmm0");
+    asm ("movups %0, %%xmm0\n\t" : : "m"(t[0]) : "xmm0");
     _sse_transform_to_vector (result.begin());
     return (result);
 }
 
 template <>
-inline matrix<4,4,float> operator* (const matrix<4,4,float>& m1, const matrix<4,4,float>& m2)
+static matrix<4,4,float> operator* (const matrix<4,4,float>& m1, const matrix<4,4,float>& m2)
 {
     matrix<4,4,float> result;
     _sse_load_matrix (m2.begin());
-    asm volatile ("movups %0, %%xmm0\n\t" : : "m"(m1[0][0]) : "xmm0");
+    asm ("movups %0, %%xmm0\n\t" : : "m"(m1[0][0]) : "xmm0");
     _sse_transform_to_vector (result[0]);
-    asm volatile ("movups %0, %%xmm0\n\t" : : "m"(m1[1][0]) : "xmm0");
+    asm ("movups %0, %%xmm0\n\t" : : "m"(m1[1][0]) : "xmm0");
     _sse_transform_to_vector (result[1]);
-    asm volatile ("movups %0, %%xmm0\n\t" : : "m"(m1[2][0]) : "xmm0");
+    asm ("movups %0, %%xmm0\n\t" : : "m"(m1[2][0]) : "xmm0");
     _sse_transform_to_vector (result[2]);
-    asm volatile ("movups %0, %%xmm0\n\t" : : "m"(m1[3][0]) : "xmm0");
+    asm ("movups %0, %%xmm0\n\t" : : "m"(m1[3][0]) : "xmm0");
     _sse_transform_to_vector (result[3]);
     return (result);
 }
@@ -146,50 +145,54 @@ inline matrix<4,4,float> operator* (const matrix<4,4,float>& m1, const matrix<4,
 
 /// Specialization for 4-component vector transform, the slow part of 3D graphics.
 template <>
-inline tuple<4,float> operator* (const tuple<4,float>& t, const matrix<4,4,float>& m)
+static tuple<4,float> operator* (const tuple<4,float>& t, const matrix<4,4,float>& m)
 {
     tuple<4,float> result;
     // This is taken from "AMD Athlon Code Optimization Guide" from AMD. 18 cycles!
     // If you are writing a 3D engine, you may want to copy it instead of calling it
     // because of the femms instruction at the end, which takes 2 cycles.
-    asm volatile (
-	"movq	   (%0), %%mm0		\n\t"	//            y | x
-	"movq	   8(%0), %%mm1		\n\t"	//            w | z
+    asm (
+	"movq	   %2, %%mm0		\n\t"	//            y | x
+	"movq	   %3, %%mm1		\n\t"	//            w | z
 	"movq	   %%mm0, %%mm2		\n\t"	//            y | x
-	"movq	   (%1), %%mm3		\n\t"	//      m[0][1] | m[0][0]
+	"movq	   %4, %%mm3		\n\t"	//      m[0][1] | m[0][0]
 	"punpckldq  %%mm0, %%mm0	\n\t"	//            x | x
-	"movq	   16(%1), %%mm4	\n\t"	//      m[1][1] | m[1][0]
+	"movq	   %6, %%mm4		\n\t"	//      m[1][1] | m[1][0]
 	"pfmul	   %%mm0, %%mm3		\n\t"	//    x*m[0][1] | x*m[0][0]
 	"punpckhdq  %%mm2, %%mm2	\n\t"	//            y | y
 	"pfmul	   %%mm2, %%mm4		\n\t"	//    y*m[1][1] | y*m[1][0]
-	"movq	   8(%1), %%mm5		\n\t"	//      m[0][3] | m[0][2]
-	"movq	   24(%1), %%mm7	\n\t"	//      m[1][3] | m[1][2]
+	"movq	   %5, %%mm5		\n\t"	//      m[0][3] | m[0][2]
+	"movq	   %7, %%mm7		\n\t"	//      m[1][3] | m[1][2]
 	"movq	   %%mm1, %%mm6		\n\t"	//            w | z
 	"pfmul	   %%mm0, %%mm5		\n\t"	//    x*m[0][3] | v0>x*m[0][2]
-	"movq	   32(%1), %%mm0	\n\t"	//      m[2][1] | m[2][0]
+	"movq	   %8, %%mm0		\n\t"	//      m[2][1] | m[2][0]
 	"punpckldq  %%mm1, %%mm1	\n\t"	//            z | z
 	"pfmul	   %%mm2, %%mm7		\n\t"	//    y*m[1][3] | y*m[1][2]
-	"movq	   40(%1), %%mm2	\n\t"	//      m[2][3] | m[2][2]
+	"movq	   %9, %%mm2		\n\t"	//      m[2][3] | m[2][2]
 	"pfmul	   %%mm1, %%mm0		\n\t"	//    z*m[2][1] | z*m[2][0]
 	"pfadd	   %%mm4, %%mm3		\n\t"	// x*m[0][1]+y*m[1][1] | x*m[0][0]+y*m[1][0]
-	"movq	   48(%1), %%mm4	\n\t"	//      m[3][1] | m[3][0]
+	"movq	   %10, %%mm4		\n\t"	//      m[3][1] | m[3][0]
 	"pfmul	   %%mm1, %%mm2		\n\t"	//    z*m[2][3] | z*m[2][2]
 	"pfadd	   %%mm7, %%mm5		\n\t"	// x*m[0][3]+y*m[1][3] | x*m[0][2]+y*m[1][2]
-	"movq	   56(%1), %%mm1	\n\t"	//      m[3][3] | m[3][2]
+	"movq	   %11, %%mm1		\n\t"	//      m[3][3] | m[3][2]
 	"punpckhdq  %%mm6, %%mm6	\n\t"	//            w | w
 	"pfadd	   %%mm0, %%mm3		\n\t"	// x*m[0][1]+y*m[1][1]+z*m[2][1] | x*m[0][0]+y*m[1][0]+z*m[2][0]
 	"pfmul	   %%mm6, %%mm4		\n\t"	//    w*m[3][1] | w*m[3][0]
 	"pfmul	   %%mm6, %%mm1		\n\t"	//    w*m[3][3] | w*m[3][2]
 	"pfadd	   %%mm2, %%mm5		\n\t"	// x*m[0][3]+y*m[1][3]+z*m[2][3] | x*m[0][2]+y*m[1][2]+z*m[2][2]
 	"pfadd	   %%mm4, %%mm3		\n\t"	// x*m[0][1]+y*m[1][1]+z*m[2][1]+w*m[3][1] | x*m[0][0]+y*m[1][0]+z*m[2][0]+w*m[3][0]
-	"movq	   %%mm3, (%2)		\n\t"	// store result->y | result->x
+	"movq	   %%mm3, %0		\n\t"	// store result->y | result->x
 	"pfadd	   %%mm1, %%mm5		\n\t"	// x*m[0][3]+y*m[1][3]+z*m[2][3]+w*m[3][3] | x*m[0][2]+y*m[1][2]+z*m[2][2]+w*m[3][2]
-	"movq	   %%mm5, 8(%2)		\n\t"	// store result->w | result->z
-	"femms				    "	// clear mmx state
-	:
-	: "r"(t.begin()), "r"(m.begin()), "r"(result.begin())
-	: "memory","mm0","mm1","mm2","mm3","mm4","mm5","mm6","mm7"
+	"movq	   %%mm5, %1"			// store result->w | result->z
+	: "=m"(result[0]), "=m"(result[2])
+	: "m"(t[0]), "m"(t[2]),
+	  "m"(m[0][0]), "m"(m[0][2]),
+	  "m"(m[1][0]), "m"(m[1][2]),
+	  "m"(m[2][0]), "m"(m[2][2]),
+	  "m"(m[3][0]), "m"(m[3][2])
+	: "mm0","mm1","mm2","mm3","mm4","mm5","mm6","mm7"
     );
+    simd::reset_mmx();
     return (result);
 }
 
@@ -197,13 +200,11 @@ inline tuple<4,float> operator* (const tuple<4,float>& t, const matrix<4,4,float
 
 /// Specialization for 4-component vector transform, the slow part of 3D graphics.
 template <>
-inline tuple<4,float> operator* (const tuple<4,float>& t, const matrix<4,4,float>& m)
+static tuple<4,float> operator* (const tuple<4,float>& t, const matrix<4,4,float>& m)
 {
     tuple<4,float> tr;
-    tr[0] = t[0] * m[0][0] + t[1] * m[1][0] + t[2] * m[2][0] + t[3] * m[3][0];
-    tr[1] = t[0] * m[0][1] + t[1] * m[1][1] + t[2] * m[2][1] + t[3] * m[3][1];
-    tr[2] = t[0] * m[0][2] + t[1] * m[1][2] + t[2] * m[2][2] + t[3] * m[3][2];
-    tr[3] = t[0] * m[0][3] + t[1] * m[1][3] + t[2] * m[2][3] + t[3] * m[3][3];
+    for (uoff_t i = 0; i < 4; ++ i)
+	tr[i] = t[0] * m[0][i] + t[1] * m[1][i] + t[2] * m[2][i] + t[3] * m[3][i];
     return (tr);
 }
 
